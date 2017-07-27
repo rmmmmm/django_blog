c284ef027ca2fa3672c15196d4360c8356f4b772
rm <596483975@qq.com>
2017-07-27 19:45:21 +0800

    新建文章和修改文章

--- blog/views.py
+++ blog/views.py
@@ -2,20 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 from . import models
+
+
 def index(request):
     articles = models.Article.objects.all()
-    return render(request, 'blog/index.html',{'articles':articles})
+    return render(request, 'blog/index.html', {'articles': articles})
 
-def article_page(request,article_id):
+
+def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, "blog/article_page.html", {'article': article})
 
-def edit_page(request):
-    return render(request, 'blog/edit_page.html')
+
+def edit_page(request, article_id):
+    if str(article_id) == '0':
+        return render(request, 'blog/edit_page.html', {'article': article})
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        return render(request, 'blog/edit_page.html')
+
 
 def edit_action(request):
-    title = request.POST.get('title','TITLE')
-    content = request.POST.get('content','CONTENT')
-    models.Article.objects.create(title=title,content=content)
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    models.Article.objects.create(title=title, content=content)
     articles = models.Article.objects.all()
     return render(request, 'blog/index.html', {'articles': articles})
