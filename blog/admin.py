9d16f8938dc6c5dbbd50f957723cffe0fe9a60e5
rm <596483975@qq.com>
2017-07-28 23:59:27 +0800

    finally

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from blog.models import Article
-admin.site.register(Article)
+
+# class 继承自admin.ModelAdmin是为了增强admin功能
+
+
+class ArticleAdmain(admin.ModelAdmin):
+    # admin中默认显示一个字段title，设置list_display是为了显示其他字段
+    list_display = ('title', 'content', 'date')
+    # 因为是tuple所以后面加‘，’ 设置list_filter是用来根据date删选admin中的数据内容的
+    list_filter = ('title', )
+
+
+admin.site.register(Article, ArticleAdmain)
